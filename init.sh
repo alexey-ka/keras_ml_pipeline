da6656cd3184d13c0c442935a40ff4d534e335cd
alexey-ka <aleksei.karetnikov@est.fib.upc.edu>
2021-04-06 02:15:32 +0200

    changed script

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@
 sed 's/ \{1,\}/ /g' ./db_init/auto-mpg.data>./db_init/auto-mpg2.data
 sed $'s/ *\t/ /g' ./db_init/auto-mpg2.data > ./db_init/auto-mpg.data
 rm ./db_init/auto-mpg2.data
-#docker-compose up -d
+docker build -t ml_pipeline_a .
+docker-compose up -d
